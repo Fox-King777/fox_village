f48a1778ae4d4d678272c01af4a9ff50bbb6dbb1
Jiang Chen <criver@moveworks.ai>
2020-10-10 17:57:48 -0700

    Fix lots of triangles

--- code/lots_of_triangles.cpp
+++ code/lots_of_triangles.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <vector>
+#include <algorithm>
 #include <cmath>
 #include <complex>
-#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-bool complex_cmp(complex<double> a, complex<double> b)
-{
-    if (real(a) == real(b))
-        return imag(a) < imag(b);
-    return real(a) < real(b);
+bool complex_cmp(complex<double> a, complex<double> b) {
+  if (real(a) == real(b)) return imag(a) < imag(b);
+  return real(a) < real(b);
 }
 
 float cross_prod(complex<float> p0, complex<float> p1) {
@@ -22,14 +20,14 @@ bool below(complex<float> a, complex<float> b, complex<float> tree) {
 }
 
 int count_below(complex<float> a, complex<float> b, vector<complex<float>>& trees) {
-    int sum = 0;
+  int sum = 0;
 
-    for (auto t : trees) {
-        if (below(a, b, t)) {
-            sum++;
-        }
+  for (auto t : trees) {
+    if (below(a, b, t)) {
+      sum++;
     }
-    return sum;
+  }
+  return sum;
 }
 
 int main() {
@@ -37,6 +35,7 @@ int main() {
   vector<complex<float>> trees;
 
   cin >> n;
+  vector<int> counts(n, 0);
 
   for (int i = 0; i < n; ++i) {
     float x, y;
@@ -44,14 +43,44 @@ int main() {
     trees.emplace_back(x, y);
   }
 
+  sort(trees.begin(), trees.end(), complex_cmp);
 
+  vector<vector<int>> belows(n, vector<int>(n));
   for (int i = 0; i < n; ++i) {
-      for (int j = i + 1; j < n; j++) {
-        cout << trees[i] << " " << trees[j] << " " << count_below(trees[i], trees[j], trees) << endl;
-      }
+    for (int j = i + 1; j < n; ++j) {
+      belows[i][j] = count_below(trees[i], trees[j], trees);
+    }
   }
 
-  /* 
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      for (int k = j + 1; k < n; ++k) {
+        auto a = trees[i];
+        auto b = trees[j];
+        auto c = trees[k];
+        int blue = belows[i][j];
+        int red = belows[j][k];
+        int black = belows[i][k];
+        int num_trees = 0;
+        if (a.real() == b.real()) {
+          num_trees = red - black - 1;
+        } else if (b.real() == c.real()) {
+          num_trees = blue - black;
+        } else if (cross_prod(a - b, b - c) < 0) {
+          num_trees = blue + red - black;
+        } else {
+          num_trees = black - blue - red - 1;
+        }
+        ++counts[num_trees];
+      }
+    }
+  }
+
+  for (int i = 0; i < n - 2; ++i) {
+    cout << counts[i] << endl;
+  }
+
+  /*
   edges of triangle:
   green: left to mid
   red: mid to right
@@ -61,7 +90,7 @@ int main() {
   /*
   4 cases:
 
-  1: blue is below green and red. /_\ 
+  1: blue is below green and red. /_\
     below green + below red - below blue = # trees in triangle
 
   2: blue is above grean and red. \-/
