a88be575c99aae536b37f33c908bee318d9cc8d0
fox_king <zheyu.ll.chen@gmail.com>
2020-11-21 18:08:03 -0800

    Add running_laps

--- code/cmp_lps.cpp
+++ code/cmp_lps.cpp
@@ -4,32 +4,32 @@
 using namespace std;
 
 vector<int> cmp_lps(char pat[], int m) {
-   vector<int> lps(m);
+  vector<int> lps(m);
 
-    int len = 0;
-    int i = 1;
+  int len = 0;
+  int i = 1;
 
-    while (i < m) {
-        if (pat[i] == pat[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len != 0) {
-                len = lps[len - 1];
-            } else {
-              lps[i] = 0;
-              i++;
-            }
-        }
+  while (i < m) {
+    if (pat[i] == pat[len]) {
+      len++;
+      lps[i] = len;
+      i++;
+    } else {
+      if (len != 0) {
+        len = lps[len - 1];
+      } else {
+        lps[i] = 0;
+        i++;
+      }
     }
-    return lps;
+  }
+  return lps;
 }
 
 int main() {
   int m = 11;
   char pat[m] = {'A', 'A', 'B', 'A', 'A', 'C', 'A', 'A', 'B', 'A', 'A'};
-  vector<int> lps = cmp_lps(pat, m); 
+  vector<int> lps = cmp_lps(pat, m);
 
   for (int i = 0; i < m; ++i) {
     cout << lps[i] << " ";
